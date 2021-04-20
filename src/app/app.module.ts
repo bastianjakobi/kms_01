c3749e77955ce99a2f460c1f1ceab28f39b96366
orms94 <olga.romashko@mni.thm.de>
2021-04-20 17:59:39 +0200

    Added done Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DoneComponent } from './done/done.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoneComponent
   ],
   imports: [
     BrowserModule,
