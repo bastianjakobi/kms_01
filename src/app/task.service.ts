a6e91adc46635da26182c6b4f2eeadfedfc25562
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2021-04-22 12:46:04 +0200

    Hotfix

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -5,17 +5,15 @@ import {Task} from '../model/Task';
   providedIn: 'root'
 })
 export class TaskService {
-  public taskList: Task[];
-  public doneList: Task[];
+  public taskList: Task[] = [];
+  public doneList: Task[] = [];
   constructor() { }
 
   addTask(name: string, description: string, priority: string, category: string, isdone: boolean): any {
-    for (const task of this.taskList){
       let id = 1;
       let task = new Task(id, name, description, priority, category, isdone);
       this.taskList.push(task);
       id += 1;
       console.log(this.taskList);
-    }
   }
 }
